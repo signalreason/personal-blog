a6ff28776669e722e076ebf5579587601e2401ec
signalreason <dev@signalreason.com>
2025-08-31 12:39:33 -0700

    validations and defaults for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, :slug, :body_md, :status, presence: true
+
   before_validation do
-    self.slug ||= title.to_s.parameterize
+    self.slug = title.to_s.parameterize if self.slug.blank?
+    self.status = "draft" if self.status.blank?
   end
 end
