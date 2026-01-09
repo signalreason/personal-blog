8296a1f4850cf064fb77fd8dc739627cd699a760
signalreason <dev@signalreason.com>
2026-01-08 22:58:54 -0800

    Enforce published visibility

--- app/controllers/published_posts_controller.rb
+++ app/controllers/published_posts_controller.rb
@@ -1,11 +1,12 @@
 class PublishedPostsController < ApplicationController
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = admin_user? ? Post.all : Post.published
   end
 
   # GET /posts/1 or /posts/1.json
   def show
-    @post = Post.find(params.expect(:id))
+    scope = admin_user? ? Post.all : Post.published
+    @post = scope.find(params.expect(:id))
   end
 end
