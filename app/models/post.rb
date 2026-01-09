e14842c444e8e3ce454de73ed361441bb4893baa
signalreason <dev@signalreason.com>
2026-01-08 22:59:15 -0800

    Constrain post status

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,24 @@
 class Post < ApplicationRecord
-  validates :title, :slug, :body_md, :status, presence: true
+  STATUSES = %w[draft published].freeze
 
-  before_validation do
-    self.slug = title.to_s.parameterize if self.slug.blank?
-    self.status = "draft" if self.status.blank?
+  validates :title, :slug, :body_md, presence: true
+  validates :status, presence: true, inclusion: { in: STATUSES }
+
+  before_validation :apply_defaults
+  before_validation :set_published_at, if: :publishing_without_timestamp?
+
+  private
+
+  def apply_defaults
+    self.slug = title.to_s.parameterize if slug.blank?
+    self.status = "draft" if status.blank?
+  end
+
+  def publishing_without_timestamp?
+    status == "published" && published_at.blank?
+  end
+
+  def set_published_at
+    self.published_at = Time.current
   end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "status must be draft or published" do
+    post = Post.new(title: "Hello", body_md: "Body", status: "archived")
+
+    assert_not post.valid?
+    assert_includes post.errors[:status], "is not included in the list"
+  end
+
+  test "publishing a post sets published_at when blank" do
+    post = Post.create!(title: "Hello", body_md: "Body", status: "draft")
+
+    assert_nil post.published_at
+
+    post.update!(status: "published")
+    assert_not_nil post.published_at
+  end
 end
